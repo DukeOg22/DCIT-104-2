0a1db8ab0015b1572555363df72336955cf5712b
DukeOg22 <102636911+DukeOg22@users.noreply.github.com>
2022-08-08 09:52:12 +0000

    Add files via upload

---
+++ DCIT 104 Assignment.py
@@ -0,0 +1,9 @@
+n = 10000
+number_of_even_numbers = n/2
+print(number_of_even_numbers)
+sum_of_even_numbers = n *((n + 1)/ 2)
+print(sum_of_even_numbers)
+average = (sum_of_even_numbers / number_of_even_numbers)
+print(average)
+
+
